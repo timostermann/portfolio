f8e3ade6355b68ab253cf4794b4946ca5dc394c9
Tim Ostermann <tim.ostermann@i22.de>
2023-03-07 21:19:49 +0100

    style: add main colors and base body styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {}
+		colors: {
+			amber: {
+				500: "#feb21e"
+			}
+		},
+		extend: {
+			colors: {
+				primary: {
+					400: "#48232e",
+					500: "#301925",
+					900: "#1C161E"
+				}
+			}
+		}
 	},
 	plugins: []
 };
