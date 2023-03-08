1a98205a862eab2aa9ffc773f2686a8fd4ef61e0
Tim Ostermann <tim.ostermann@i22.de>
2023-03-08 20:30:33 +0100

    style: add tab styling

--- src/components/tabs.svelte
+++ src/components/tabs.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
 	import type { NavItem } from "../types";
-	import cn from "classnames";
 	import { onMount } from "svelte";
+	import cn from "classnames";
 
 	export let tabItems: NavItem[];
 
 	let activeTabIndex = 0;
 	let tabElements: HTMLDivElement[] = Array(tabItems.length).fill(null);
 	let tabIndicator: HTMLDivElement;
-
-	const getActiveElement = () =>
-		tabElements.find((_, index) => index === activeTabIndex) ?? tabElements[0];
 
 	const setTabIndicatorPosition = () => {
 		if (!tabIndicator) return;
@@ -27,7 +24,7 @@
 <div class="relative flex gap-4 xs:gap-10">
 	{#each tabItems as tab, index}
 		<div
-			class={cn("px-7", { "active class": index === activeTabIndex })}
+			class={cn("px-6", { "active class": index === activeTabIndex })}
 			bind:this={tabElements[index]}
 		>
 			<a
@@ -41,7 +38,7 @@
 		</div>
 	{/each}
 	<div
-		class="absolute bottom-0 left-0 h-[2px] w-[100px] origin-left rounded-sm bg-amber-500 transition-transform duration-500 ease-in-out"
+		class="tab-shadow absolute -bottom-4 left-0 h-[2px] w-[100px] origin-left rounded-sm bg-amber-500 transition-transform duration-500 ease-in-out"
 		bind:this={tabIndicator}
 	/>
 </div>
