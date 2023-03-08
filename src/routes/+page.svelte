ccda5559f7addcffa4f2bc12def4de2852b42e1a
Tim Ostermann <tim.ostermann@i22.de>
2023-03-08 22:09:08 +0100

    feat: add medium sized stage image variant

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import stage from "assets/stage.png";
+	import stageLg from "assets/stage-lg.png";
 	import stageXl from "assets/stage-xl.png";
 </script>
 
 <picture>
 	<source srcset={stageXl} media="(min-width: 1500px)" />
-	<img
-		src={stage}
-		alt="Laptop on a desk"
-		class="h-[500px] max-h-screen w-full object-cover xs:h-auto"
-	/>
+	<source srcset={stageLg} media="(min-width: 1100px)" />
+	<img src={stage} alt="Laptop on a desk" class="h-screen w-full object-cover " />
 </picture>
 <h1>About</h1>
