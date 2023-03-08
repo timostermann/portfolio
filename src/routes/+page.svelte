6e8e441a0a1c9cb42250a28ceb509b32b150842d
Tim Ostermann <tim.ostermann@i22.de>
2023-03-08 23:01:13 +0100

    feat: add about stage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,48 @@
 <script lang="ts">
 	import stage from "assets/stage.png";
 	import stageLg from "assets/stage-lg.png";
-	import stageXl from "assets/stage-xl.png";
+	import stageXl from "assets/stage-xl.png"; // TODO: center xl image
 </script>
 
-<picture>
-	<source srcset={stageXl} media="(min-width: 1500px)" />
-	<source srcset={stageLg} media="(min-width: 1100px)" />
-	<img src={stage} alt="Laptop on a desk" class="h-screen w-full object-cover " />
-</picture>
-<h1>About</h1>
+<div class="relative">
+	<picture>
+		<source srcset={stageXl} media="(min-width: 1500px)" />
+		<source srcset={stageLg} media="(min-width: 1100px)" />
+		<img src={stage} alt="Laptop on a desk" class="h-screen w-full object-cover " />
+	</picture>
+	<div class="absolute inset-0 z-[2] flex items-center justify-center">
+		<div class="flex w-full -translate-y-10 flex-col px-4 drop-shadow-md xs:w-[400px]">
+			<p class="font-extralight">Hi, I am</p>
+			<h1 class="mb-4 text-4xl font-semibold">Tim Ostermann</h1>
+			<p>
+				<span class="inline-block pb-2"
+					>I'm a front-end developer with an eye for good design and a heart for clean code moving
+					towards the goal of being a true fullstack.</span
+				>
+				<span class="inline-block pb-4"
+					>I'm based in Nuremberg and currently working remotely for
+					<a
+						class="font-semibold text-amber-500 transition-colors hover:text-amber-600"
+						href="https://i22.de"
+						target="_blank"
+						rel="noreferrer">i22 Digitalagentur</a
+					>.</span
+				>
+				If you want to get in contact, check out
+				<a
+					class="font-semibold text-amber-500 transition-colors hover:text-amber-600"
+					href="https://www.xing.com/profile/Tim_Ostermann13/cv"
+					target="_blank"
+					rel="noreferrer">Xing</a
+				>
+				or
+				<a
+					class="font-semibold text-amber-500 transition-colors hover:text-amber-600"
+					href="https://www.linkedin.com/in/tim-ostermann-b52a501a1/"
+					target="_blank"
+					rel="noreferrer">LinkedIn</a
+				>.
+			</p>
+		</div>
+	</div>
+</div>
