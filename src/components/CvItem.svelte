c5d497f79261b25a9fd6277f83367ca8f96c3d27
Tim Ostermann <tim.ostermann@i22.de>
2023-03-23 21:37:12 +0100

    feat: add cv data

--- src/components/CvItem.svelte
+++ src/components/CvItem.svelte
@@ -11,13 +11,15 @@
 <div
 	class={cn(
 		"flex max-w-md flex-col gap-1 text-base font-thin",
-		reverse ? "items-end" : "items-start"
+		reverse ? "items-end text-end" : "items-start"
 	)}
 >
 	<p class="text-sm">{cvItem.date}</p>
 	<h2 class="text-xl font-medium">{cvItem.title}</h2>
-	<p class="markdown mt-1"><SvelteMarkdown source={cvItem.description} isInline /></p>
-	<div class="mt-3 flex gap-2">
+	<p class="markdown mt-1 flex flex-col gap-2">
+		<SvelteMarkdown source={cvItem.description} isInline />
+	</p>
+	<div class="mt-3 flex flex-wrap gap-2">
 		{#each cvItem.technologies as tag}
 			<TagPill {tag} />
 		{/each}
--- src/data/cvData.ts
+++ src/data/cvData.ts
@@ -4,7 +4,38 @@ export const cvItems: CvItem[] = [
 	{
 		date: "10/2018 - 01/2022",
 		title: "Bachelor of Science in Media Informatics",
-		description: `This is a short description of what I learned. [Learn More](https://www.w-hs.de/medieninformatik-ge/) \n\nWestfälische Hochschule Gelsenkirchen`,
-		technologies: ["Node.js", "Express.js"]
+		description:
+			"In my studies I learned the basics of computer science and programming. Because of the focus on media, I also learned about design and animation in 2D and 3D. \n\n [Westfälische Hochschule Gelsenkirchen](https://www.w-hs.de/medieninformatik-ge/)",
+		technologies: ["Java", "C", "SQL", "REST", "Git"]
+	},
+	{
+		date: "10/2019 - 12/2020",
+		title: "Working Student Software Development",
+		description:
+			"In my first job as a developer I worked on internal tools for the company. Here I learned a lot about web technologies and testing. It was very educational to get my first experiences with real-world code. \n\n [ZGS Bildungs-GmbH (Schülerhilfe)](https://www.schuelerhilfe.de)",
+		technologies: ["JavaScript", "HTML", "CSS", "Cypress"]
+	},
+	{
+		date: "02/2021 - 11/2021",
+		title: "Working Student Software Development",
+		description:
+			"I had the chance to work for a software agency with great colleagues and a focus on skills development and high coding standards. Besides doing first steps in the world of React, I also learned a lot about app development, agile project management processes and the benefits of clean code. \n\n [BRICKMAKERS GmbH](https://www.brickmakers.de)",
+		technologies: ["React", "TypeScript", "Dart", "Flutter", "Jest", "Scrum"]
+	},
+	{
+		date: "12/2021 - present",
+		title: "Frontend Developer",
+		description:
+			"In my first full-time job, I work on various frontend projects for a variety of clients. I work in a small autonomous team with high standards for code quality, maintainability, performance and accessibility. We mainly work on projects with Headless CMS integrations and a focus on beautiful design. \n\n [i22 Digitalagentur GmbH](https://www.i22.de)",
+		technologies: [
+			"React",
+			"TypeScript",
+			"Next.js",
+			"Jest",
+			"Tailwind",
+			"Headless CMS",
+			"GraphQL",
+			"gsap"
+		]
 	}
 ];
