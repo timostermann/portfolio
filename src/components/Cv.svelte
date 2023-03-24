f5cdfbc70ca95eda06a1871c00062baa729fd207
Tim Ostermann <tim.ostermann@i22.de>
2023-03-24 10:52:11 +0100

    feat: add desktop cv version

--- src/components/Cv.svelte
+++ src/components/Cv.svelte
@@ -7,13 +7,31 @@
 </script>
 
 <div class="flex w-full items-center justify-center pr-4">
-	<div class="relative grid grid-cols-[32px_1fr] justify-items-center gap-y-16 gap-x-4 sm:gap-x-6">
-		{#each cvItems as cvItem}
-			<TimelineMarker size="small" />
+	<div
+		class="relative grid grid-cols-[32px_1fr] justify-items-center gap-y-16 gap-x-4 sm:gap-x-6 xl:hidden"
+	>
+		{#each cvItems as cvItem, index}
+			<TimelineMarker size={index === 0 ? "large" : "small"} />
 			<CvItem {cvItem} reverse={false} />
 		{/each}
 		<div
 			class="absolute left-[14px] h-[105%] w-1 rounded-xl bg-gradient-to-b from-amber-500 via-amber-500"
+		/>
+	</div>
+	<div class="relative hidden grid-cols-[1fr_32px_1fr] justify-items-center gap-x-6 xl:grid">
+		{#each cvItems as cvItem, index}
+			{#if index % 2 === 0}
+				<div />
+				<TimelineMarker size={index === 0 ? "large" : "small"} />
+				<CvItem {cvItem} reverse={false} />
+			{:else}
+				<CvItem {cvItem} reverse={true} />
+				<TimelineMarker size="small" />
+				<div />
+			{/if}
+		{/each}
+		<div
+			class="absolute left-0 right-0 mx-auto h-[105%] w-1 rounded-xl bg-gradient-to-b from-amber-500 via-amber-500"
 		/>
 	</div>
 </div>
