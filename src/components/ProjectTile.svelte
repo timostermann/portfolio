76347ccf7dd4b2ced8077f23a1eaf9197a381bfa
Tim Ostermann <tim.ostermann@i22.de>
2023-03-25 21:59:05 +0100

    feat: add project tile frame

---
+++ src/components/ProjectTile.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<article
+	class="h-96 w-full rounded-xl bg-gradient-to-br from-[rgb(254_178_30_/_0.2)] to-[rgb(254_178_30_/_0.4)] p-4 shadow-[0_4px_30px_rgb(0_0_0_/_0.1)] backdrop-blur-md"
+/>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import ProjectTile from "components/ProjectTile.svelte";
 </script>
 
-<section class="relative flex justify-center pt-24">
-	<div class="w-[400px] max-w-full">
+<section
+	class="relative flex flex-col items-center bg-gradient-to-b from-primary-900 via-primary-500 to-primary-900 px-8 pt-24"
+>
+	<div class="w-[500px] max-w-full">
 		<h1 class="mb-4 text-4xl font-extralight">Project Overview</h1>
 		<p>
 			<span class="inline-block pb-4"
@@ -13,7 +16,11 @@
 				are only projects I did for for fun and/or to try out new technologies.</span
 			>
 			I hope the list still gives you a good overview of my skills (and why I'd rather work with a talented
-			designer than rely on my own design skills).
+			designer than rely on my own design skills). You can also check out the source code on my GitHub
+			and find out, that I have a lot of READMEs yet to write...
 		</p>
+	</div>
+	<div class="mt-12 flex w-[700px] max-w-full flex-col items-center gap-8">
+		<ProjectTile />
 	</div>
 </section>
