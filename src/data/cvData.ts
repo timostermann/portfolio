732a5c3b7fa410c3468f0b623e55ccaa0a663fbc
Tim Ostermann <tim.ostermann@i22.de>
2023-03-27 21:40:35 +0200

    content: text improvements

--- src/data/cvData.ts
+++ src/data/cvData.ts
@@ -5,7 +5,7 @@ export const cvItems: CvItem[] = [
 		date: "10/2018 - 01/2022",
 		title: "Bachelor of Science in Media Informatics",
 		description:
-			"In my studies at the University of Applied Science in Gelsenkirchen I learned the basics of computer science and programming. Because of the focus on media, I also learned about design and animation in 2D and 3D. \n\n [More about the study program](https://www.w-hs.de/medieninformatik-ge/)",
+			"During my studies at the University of Applied Sciences in Gelsenkirchen I learned the basics of computer science and programming. Because of the focus on media, I also learned about design and animation in 2D and 3D. \n\n [More about the study program](https://www.w-hs.de/medieninformatik-ge/)",
 		technologies: ["Java", "C", "SQL", "REST", "Git"]
 	},
 	{
@@ -26,7 +26,7 @@ export const cvItems: CvItem[] = [
 		date: "12/2021 - present",
 		title: "Frontend Developer",
 		description:
-			"In my first full-time job, I work on various frontend projects for a variety of clients. I work in a small autonomous team with high standards for code stability, maintainability, performance and accessibility. We mainly work on projects with Headless CMS integrations and a focus on beautiful design. \n\n [i22 Digitalagentur GmbH](https://www.i22.de)",
+			"In my first full-time job, I work on various frontend projects for a variety of clients. I work in a small autonomous team with high standards for code stability, maintainability, performance and accessibility. We mainly work on projects with headless CMS integrations and a focus on beautiful design. \n\n [i22 Digitalagentur GmbH](https://www.i22.de)",
 		technologies: [
 			"React",
 			"TypeScript",
--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
 		date: "2023",
 		title: "Personal Website",
 		description:
-			"After sitting on the idea and an unused domain for a while, I finally found the time and motivation to build this website. I used it as an opportunity to dive into some new technologies. I've used Svelte before, but this is the first time I used SvelteKit. I used Midjourney to create a stage image, Figma to draft a design and Tailwind for styling.",
+			"After sitting on the idea and an unused domain for a while, I finally found the time and motivation to build this website. I used it as an opportunity to dive into some new technologies. I've used Svelte before, but this is the first time I've used SvelteKit. I used Midjourney to create a stage image, Figma to draft a design and Tailwind for styling.",
 		technologies: ["Svelte", "SvelteKit", "Tailwind"],
 		source: "https://github.com/timostermann/portfolio"
 	},
@@ -13,7 +13,7 @@ export const projects: Project[] = [
 		date: "2022",
 		title: "ViCat Meme Coin Website",
 		description:
-			"ViCat is a meme coin project two friends and I are working on. While I don't have any experience (or honestly knowledge) about crypto, I've been having fun creating websites for it. I did a MVP website and some smaller event websites for it with React in 2021, but after a redesign and a new logo, I decided to rebuild the website with Svelte and SCSS. It's been a cool playground and really made me appreciate the simplicity of Svelte.",
+			"ViCat is a meme coin project that two friends and I are working on. While I have no experience (or knowledge, honestly) with crypto, I've had fun making websites for it. I did an MVP site and some smaller event sites for it with React in 2021, but after a redesign and a new logo, I decided to rebuild the site with Svelte and SCSS. It's been a cool playground and really made me appreciate the simplicity of Svelte.",
 		technologies: ["Svelte", "SCSS"],
 		source: "https://github.com/timostermann/vicat-v2",
 		link: "https://vicat.tech"
@@ -22,7 +22,7 @@ export const projects: Project[] = [
 		date: "2021",
 		title: "SKM Measurement App",
 		description:
-			"I worked on a measurement app for shower cabin builders. The project was part of my bachelor thesis. It was my first time working with Flutter and Dart. I really enjoyed the experience and I would love to dive deeper into app development at some point.",
+			"I was working on a measurement app for shower manufacturers. The project was part of my bachelor thesis. It was my first time working with Flutter and Dart. I really enjoyed the experience and would like to dive deeper into app development at some point.",
 		technologies: ["Flutter", "Dart"],
 		source: "https://github.com/timostermann/skm"
 	},
@@ -30,7 +30,7 @@ export const projects: Project[] = [
 		date: "2020",
 		title: "Huffman Compression",
 		description:
-			"To also present a less visual project, I decided to add a project that I did in university. It's an implementation of the Huffman compression algorithm in C. It was a great project to learn more about low-level programming and how to do a CLI application.",
+			"To also present a less visual project, I decided to add a project I did in university. It's an implementation of the Huffman compression algorithm in C. It was a great project to learn more about low-level programming and how to make a CLI application.",
 		technologies: ["C"],
 		source: "https://github.com/timostermann/huffman"
 	}
